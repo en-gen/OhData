c77f8d73f67edef268b1937ac793a228f70ba23e
Brian Enegn <engenb@gmail.com>
2022-02-04 01:49:35 -0600

    basic concepts of entity set profile
    still needs some work.  will need to do a little logical gymnastics in order to properly configure the return types of functions and actions, but should be possible
    
    next steps will involve mapping EDM to endpoints

---
+++ src/OhData.Abstractions/OhDataContext.cs
@@ -0,0 +1,7 @@
+﻿namespace OhData.Abstractions;
+
+public sealed class OhDataContext
+{
+    // TODO: WIP - just an example
+    internal IEnumerable<Type> RegisteredModelTypes { get; set; }
+}
---
+++ src/OhData.Abstractions/OhDataOptions.cs
@@ -0,0 +1,6 @@
+﻿namespace OhData.Abstractions;
+
+public class OhDataOptions
+{
+    public bool CamelCaseSerialization { get; set; } = true;
+}
