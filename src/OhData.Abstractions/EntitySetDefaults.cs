b07607efbf80d598d053774d521e5af07bd0f78c
Brian Enegn <engenb@gmail.com>
2022-02-04 01:49:35 -0600

    basic concepts of entity set profile
    still needs some work.  will need to do a little logical gymnastics in order to properly configure the return types of functions and actions, but should be possible
    
    next steps will involve mapping EDM to endpoints

---
+++ src/OhData.Abstractions/EntitySetDefaults.cs
@@ -0,0 +1,10 @@
+﻿namespace OhData.Abstractions;
+
+public class EntitySetDefaults
+{
+    public bool SelectEnabled { get; set; }
+    public bool ExpandEnabled { get; set; }
+    public bool FilterEnabled { get; set; }
+    public bool OrderByEnabled { get; set; }
+    public bool CountEnabled { get; set; } 
+}
---
+++ src/OhData.TestBench.AspNetCore/Samples.cs
@@ -0,0 +1,62 @@
+﻿using OhData.Abstractions;
+
+namespace OhData.TestBench.AspNetCore;
+
+public class Parent
+{
+    public Guid Id { get; set; }
+
+    public Parent Peer { get; set; }
+
+    public IEnumerable<Child> Children { get; set; }
+}
+
+public class ParentEntitySetProfile : EntitySetProfile<Guid, Parent>
+{
+    public ParentEntitySetProfile() : base(x => x.Id)
+    {
+        SelectEnabled = true;
+        FilterEnabled = true;
+        ExpandEnabled = true;
+        OrderByEnabled = true;
+        CountEnabled = true;
+
+        HasOptional(x => x.Peer);
+        HasMany(x => x.Children);
+
+        BindFunction(ShouldNotHaveSideEffects);
+        BindAction(HasSideEffects);
+    }
+
+    private async Task<IEnumerable<Parent>> ShouldNotHaveSideEffects(string p1, int p2, long p3)
+    {
+        return Array.Empty<Parent>();
+    }
+
+    private void HasSideEffects(){}
+        
+}
+
+public class Child
+{
+    public Guid Id { get; set; }
+
+    public Parent Parent { get; set; }
+
+    public IEnumerable<Child> Siblings { get; set; }
+}
+
+public class ChildEntitySetProfile : EntitySetProfile<Guid, Child>
+{
+    public ChildEntitySetProfile() : base(x => x.Id)
+    {
+        SelectEnabled = true;
+        FilterEnabled = true;
+        ExpandEnabled = true;
+        OrderByEnabled = true;
+        CountEnabled = true;
+
+        HasRequired(x => x.Parent);
+        HasMany(x => x.Siblings);
+    }
+}
